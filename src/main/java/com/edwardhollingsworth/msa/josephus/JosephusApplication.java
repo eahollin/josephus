d374ba752de47b95f493301e7c03cdaafc579f87
ehollingsworth <ehollingsworth@IOP-949HM12.mshome.net>
2018-09-12 12:01:49 -0400

    Added basic Kafka integration for POC

--- src/main/java/com/edwardhollingsworth/msa/josephus/JosephusApplication.java
+++ src/main/java/com/edwardhollingsworth/msa/josephus/JosephusApplication.java
@@ -1,12 +1,22 @@
 package com.edwardhollingsworth.msa.josephus;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
 
 @SpringBootApplication
 public class JosephusApplication {
 
+	Logger log = LogManager.getLogger(com.edwardhollingsworth.msa.josephus.JosephusApplication.class);
+	
     public static void main(String[] args) {
         SpringApplication.run(JosephusApplication.class, args);
+    }
+    
+    @KafkaListener(id = "josephus", topics = "test")  //, clientIdPrefix = "josephus-app"
+    public void listen(String data) {
+        log.info("Kafka message received: " + data);
     }
 }
